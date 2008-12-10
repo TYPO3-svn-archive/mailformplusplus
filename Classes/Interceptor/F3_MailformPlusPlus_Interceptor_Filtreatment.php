16618c1c3111b297c92dc5678ed4c7a01ca3cd68
reinhardfuehricht <reinhardfuehricht@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-10 23:04:14 +0000

    Added new class for XSS removal
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mailformplusplus/trunk@14811 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/Interceptor/F3_MailformPlusPlus_Interceptor_Filtreatment.php
@@ -0,0 +1,65 @@
+<?php
+/*                                                                        *
+ * This script is part of the TYPO3 project - inspiring people to share!  *
+ *                                                                        *
+ * TYPO3 is free software; you can redistribute it and/or modify it under *
+ * the terms of the GNU General Public License version 2 as published by  *
+ * the Free Software Foundation.                                          *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
+ * Public License for more details.                                       *
+ *                                                                        */
+
+/**
+ * An interceptor doing XSS checking on GET/POST parameters
+ *
+ * @author	Reinhard Führicht <rf@typoheads.at>
+ * @package	F3_MailformPlusPlus
+ * @subpackage	Interceptor
+ */
+class F3_MailformPlusPlus_Interceptor_Filtreatment extends F3_MailformPlusPlus_AbstractInterceptor {
+	
+	/**
+     * The main method called by the controller
+     * 
+     * @author Reinhard Führicht <rf@typoheads.at>
+     * @param array $gp The GET/POST parameters
+     * @param array $settings The defined TypoScript settings for the finisher
+     * @return array The probably modified GET/POST parameters
+     */
+	public function process($gp,$settings) {
+		
+		return $this->sanitizeValues($gp);
+	}
+	
+	/**
+     * This method does XSS checks and escapes malicious data
+     * 
+     * @author Reinhard Führicht <rf@typoheads.at>
+     * @param array $values The GET/POST parameters
+     * @return array The sanitized GET/POST parameters
+     */
+	public function sanitizeValues($values) {
+		
+		if(!is_array($values)) {
+			return array();
+		}
+		
+		require_once(t3lib_extMgm::extPath('mailformplusplus')."Resources/PHP/filtreatment/Filtreatment.php");
+		$filter = new Filtreatment();
+		foreach ($values as $key => $value) {
+			if(is_array($value)) {
+				$sanitizedArray[$key] = $this->sanitizeValues($value);
+			} else {
+				$value = str_replace("\t","",$value);
+				$sanitizedArray[$key] = $filter->ft_xss($value);
+				
+			}
+		}
+		return $sanitizedArray;
+	}
+	
+}
+?>
