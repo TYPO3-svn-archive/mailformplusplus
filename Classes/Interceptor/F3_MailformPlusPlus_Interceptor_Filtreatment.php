58109cf95fda1fe0f53c9f95cb8215fc1bcf1496
reinhardfuehricht <reinhardfuehricht@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-23 21:34:36 +0000

    Added detection of a strings charset before doing XSS-removal stuff.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mailformplusplus/trunk@17147 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Interceptor/F3_MailformPlusPlus_Interceptor_Filtreatment.php
+++ Classes/Interceptor/F3_MailformPlusPlus_Interceptor_Filtreatment.php
@@ -45,20 +45,66 @@ class F3_MailformPlusPlus_Interceptor_Filtreatment extends F3_MailformPlusPlus_A
 			return array();
 		}
 		
+		
+		
 		require_once(t3lib_extMgm::extPath('mailformplusplus')."Resources/PHP/filtreatment/Filtreatment.php");
 		$filter = new Filtreatment();
 		foreach ($values as $key => $value) {
 			if(is_array($value)) {
 				$sanitizedArray[$key] = $this->sanitizeValues($value);
-			} elseif(!empty($value)) {
+			} elseif(!empty($value) && !is_numeric($value)) {
 				
 				$value = str_replace("\t","",$value);
-				$value = utf8_encode($value);
+				$isUTF8 = true;
+				if(!$this->isUTF8($value)) {
+					$isUTF8 = false;
+				}
+				
+				if(!$isUTF8) {
+					$value = utf8_encode($value);
+				}
 				$value = $filter->ft_xss($value,'UTF-8');
-				$sanitizedArray[$key] = utf8_decode($value);
+				
+				if(!$isUTF8) {
+					$value = utf8_decode($value);
+				}
+				$sanitizedArray[$key] = $value;
 			}
 		}
 		return $sanitizedArray;
+	}
+	
+	/**
+     * This method detects if a given input string if valid UTF-8.
+     * 
+     * @author hmdker <hmdker(at)gmail(dot)com>
+     * @param string 
+     * @return boolean is UTF-8
+     */
+	protected function isUTF8($str) {
+		$c=0; $b=0;
+	    $bits=0;
+	    $len=strlen($str);
+	    for($i=0; $i<$len; $i++){
+	        $c=ord($str[$i]);
+	        if($c > 128){
+	            if(($c >= 254)) return false;
+	            elseif($c >= 252) $bits=6;
+	            elseif($c >= 248) $bits=5;
+	            elseif($c >= 240) $bits=4;
+	            elseif($c >= 224) $bits=3;
+	            elseif($c >= 192) $bits=2;
+	            else return false;
+	            if(($i+$bits) > $len) return false;
+	            while($bits > 1){
+	                $i++;
+	                $b=ord($str[$i]);
+	                if($b < 128 || $b > 191) return false;
+	                $bits--;
+	            }
+	        }
+	    }
+	    return true;
 	}
 	
 }
