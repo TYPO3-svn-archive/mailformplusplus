9ed7d8492b7e5137a7823ba7fc48e1c1d2e75639
reinhardfuehricht <reinhardfuehricht@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-24 22:59:52 +0000

    Fixed small bug in function substitueIssetSubparts
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mailformplusplus/trunk@17186 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Utils/F3_MailformPlusPlus_StaticFuncs.php
+++ Classes/Utils/F3_MailformPlusPlus_StaticFuncs.php
@@ -108,7 +108,7 @@ class F3_MailformPlusPlus_StaticFuncs {
 		
 					// set nowrite flag if required until the next ISSET_xyz
 					// (only if not already set by envelop)
-					if((!$this->markersCountAsSet($markers, $matches[1])) && (!$nowrite)) {
+					if((!F3_MailformPlusPlus::markersCountAsSet($markers, $matches[1])) && (!$nowrite)) {
 						$nowrite = $matches[1];
 					}
 				} else { // close it
