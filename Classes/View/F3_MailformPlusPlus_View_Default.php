d1ceec2145579c5c3417d0c70ef69cd2014ace1d
erep <erep@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-17 18:45:57 +0000

    #2874 remove not used VALUE_-markers in fillValueMarkers() needed for nested markers
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mailformplusplus/trunk@17964 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/View/F3_MailformPlusPlus_View_Default.php
+++ Classes/View/F3_MailformPlusPlus_View_Default.php
@@ -610,6 +610,10 @@ class F3_MailformPlusPlus_View_Default extends F3_MailformPlusPlus_AbstractView 
 			} // foreach end
 		} // if end
 		$this->template = $this->cObj->substituteMarkerArray($this->template, $markers);
+
+		//remove remaining VALUE_-markers
+		//needed for nested markers like ###LLL:tx_myextension_table.field1.i.###value_field1###### to avoid wrong marker removal if field1 isn't set
+		$this->template = preg_replace('/###value_.*?###/i', '', $this->template);
 	}
 
 	/**
